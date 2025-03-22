7011682bde8f9c899421ed152a12999af1f26d61
Timothy <tcraig258@gmail.com>
2025-03-22 18:41:06 +0100

    log messages

--- tdtomonapari/napari/log.py
+++ tdtomonapari/napari/log.py
@@ -1,16 +1,66 @@
 import logging
-from qtpy.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QComboBox, QLabel
+from qtpy.QtCore import Signal, QObject
+from qtpy.QtWidgets import QWidget, QVBoxLayout, QTextEdit, QComboBox, QLabel, QProgressBar, QHBoxLayout
+from tomobase.log import tomobase_logger, logger 
 
 class LogSettingsWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.layout = QVBoxLayout(self)
+        self.layout = QHBoxLayout(self)
         self.label = QLabel('Log Level:')
         self.combo_box = QComboBox()
         self.combo_box.addItems(["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.combo_box)
         self.setLayout(self.layout)
+
+class ProgressInfoWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.label = QLabel('Progress:')
+        self.label_task = QLabel('')
+        self.label_eta = QLabel('')
+
+        self.layout = QHBoxLayout(self)
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.label_task)
+        self.layout.addWidget(self.label_eta)
+
+class ProgressWidget(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.layout = QVBoxLayout(self)
+        self.info = ProgressInfoWidget()
+
+        self.progress = tomobase_logger.progress_bar
+        self.progress_bar = QProgressBar(self)
+        self.progress_bar.setRange(0, 100)
+        self.layout.addWidget(self.info)
+        self.layout.addWidget(self.progress_bar)
+        self.setLayout(self.layout)
+
+        self.progress.started.connect(self.start)
+        self.progress.updated.connect(self.update)
+        self.progress.finished.connect(self.finish)
+        self.progress.maxupdated.connect(self.progress_bar.setMaximum)    
+
+    def setMaximum(self, max_value):
+        self.progress_bar.setRange(0, max_value)
+        
+    def start(self, max_value, task):
+        self.progress_bar.setRange(0, max_value)
+        self.progress_bar.setValue(0)
+        self.info.label_task.setText(task)
+
+    def update(self, value, eta):
+        self.progress_bar.setValue(value)
+        self.info.label_eta.setText(eta)
+
+    def finish(self):
+        self.progress_bar.setValue(0)
+        self.info.label_task.setText('')
+        self.info.label_eta.setText('')
+    
 
 class QTextEditLogger(logging.Handler):
     def __init__(self, text_edit):
@@ -22,6 +72,8 @@ class QTextEditLogger(logging.Handler):
         self.text_edit.append(msg)
 
 class LogWidget(QWidget):
+    progress_updated = Signal(int, int)  # Signal to update progress bar
+
     def __init__(self, parent=None):
         super().__init__(parent)
         
@@ -29,33 +81,27 @@ class LogWidget(QWidget):
 
         self.settings_widget = LogSettingsWidget()
         self.log_text_edit = QTextEdit(self)
-        self.log_text_edit.setReadOnly(True) 
+        self.log_text_edit.setReadOnly(True)
+        self.progress_widget = ProgressWidget(self)
         
         self.layout.addWidget(self.settings_widget)
         self.layout.addWidget(self.log_text_edit)
+        self.layout.addWidget(self.progress_widget)
         self.setLayout(self.layout)
         
         self.log_handler = QTextEditLogger(self.log_text_edit)
         self.log_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
         
-        # Get the logger and add the custom handler
-        self.logger = logging.getLogger('tomobase_logger')
-        self.logger.addHandler(self.log_handler)
-        self.logger.setLevel(logging.DEBUG)
+        self.logger = logger
+
+        # Ensure no duplicate handlers
+        if not any(isinstance(handler, QTextEditLogger) for handler in self.logger.handlers):
+            self.logger.addHandler(self.log_handler)
         
         self.settings_widget.combo_box.currentIndexChanged.connect(self.onchange_loglevel)
 
     def onchange_loglevel(self, index):
         log_level = self.settings_widget.combo_box.currentText()
-        match log_level:
-            case "DEBUG":
-                self.logger.setLevel(logging.DEBUG)
-            case "INFO":
-                self.logger.setLevel(logging.INFO)
-            case "WARNING":
-                self.logger.setLevel(logging.WARNING)
-            case "ERROR":
-                self.logger.setLevel(logging.ERROR)
-            case "CRITICAL":
-                self.logger.setLevel(logging.CRITICAL)
+        self.logger.setLevel(getattr(logging, log_level))
+
 
