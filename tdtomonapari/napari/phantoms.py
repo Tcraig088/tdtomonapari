66bb21832ff48db971f607d2a497046ebc3c420d
Timothy <tcraig258@gmail.com>
2025-05-28 14:04:46 +0200

    update linking phantom protocol

--- tdtomonapari/napari/phantoms.py
+++ tdtomonapari/napari/phantoms.py
@@ -2,6 +2,7 @@ from functools import partial
 from typing import List
 from napari.types import LayerData
 from tomobase.log import logger
+from tomobase.globals import TOMOBASE_PHANTOMS
 from tomobase.tiltschemes import GRS, Incremental
 from tomobase import phantoms
 from tomobase import processes
@@ -22,8 +23,9 @@ class PhantomSelectWidget(QDialog):
         
         self.phantom_select = QComboBox()
         self.phantom_select.addItem('Select Phantom')
+
         for key, item in TOMOBASE_PHANTOMS.items():
-            self.combobox_select.addItem(item.name.replace('_', ' ').capitalize())
+            self.phantom_select.addItem(item.name.replace('_', ' ').capitalize())
         
         self.phantom_widget = QWidget()
         self.confirm_button = QPushButton('Confirm')   
@@ -36,8 +38,8 @@ class PhantomSelectWidget(QDialog):
 
         
         self.setLayout(self.layout)
-        
-        self.confirm_button.clicked.connect(self.parse_phantom)
+        self.phantom_select.currentIndexChanged.connect(self.onComboboxChange)
+        self.confirm_button.clicked.connect(self.Parse)
 
     def onComboboxChange(self, index):
         self.layout.removeWidget(self.phantom_widget)
@@ -45,15 +47,43 @@ class PhantomSelectWidget(QDialog):
         self.phantom_widget = None
 
         if self.phantom_select.currentIndex() > 0:
-            process = TOMOBASE_PHANTOMS[self.phantom_select.currentText()]  
-            phantom = build_phantoms_widget(self.phantom_select.currentText())
-            self.phantom_widget = phantom.viewer()
+            logger.info(f"Selected phantom: {self.phantom_select.currentText()}")
+            process = TOMOBASE_PHANTOMS[self.phantom_select.currentText().replace(' ', '_').upper()]  
+            phantom = build_phantoms_widget(process.value)
+            self.phantom_widget = phantom.native
+            self.layout.addWidget(self.phantom_widget, 1, 0, 1, 2)
         else:
             self.phantom_widget = QWidget()
         self.layout.addWidget(self.phantom_widget, 1, 0, 1, 2)  
 
+    def Parse(self):
+        """Parse the selected phantom and return the corresponding layer data."""
+        selected_phantom = self.phantom_select.currentText().replace(' ', '_').upper()
+        if selected_phantom == 'Select Phantom':
+            logger.warning("No phantom selected.")
+            return
+        
+        phantom = TOMOBASE_PHANTOMS[selected_phantom]
+
+        # Get the function associated with the selected phantom
+        phantom_function = TOMOBASE_PHANTOMS[selected_phantom]
+        
+        # Call the function to get the layer data
+        layer_data = phantom_function()
+        
+        self.accept()
+
 def build_phantoms_widget(function):
     return magicgui.magicgui(function)
+
+def load_phantom() -> List[LayerData]:
+    """Load a phantom from the list of available phantoms."""
+    viewer = napari.current_viewer()
+    phantom_widget = PhantomSelectWidget(viewer=viewer)
+    phantom_widget.exec_()
+
+    selected_phantom = phantom_widget.Parse()
+    return selected_phantom
 
 def _load_data(name, **kwargs) -> List[LayerData]:
     match name:
